3f32a64a8b98f35eed7b70aea92655bae341b50c
Eko Aryanto <ekoaryanto11@gmail.com>
2024-02-10 21:24:22 +0700

    day 6/365

---
+++ 6-dnaToRna.js
@@ -0,0 +1,28 @@
+// WPU CODING CHALLANGE
+// DAY 6/365
+
+// function DNAtoRNA(dna) {
+//     let rna = '';
+//     for (let i = 0; i < dna.length; i++) {
+//         // if (dna[i] === 'T') {
+//         //     rna += 'U';
+//         // } else {
+//         //     rna += dna[i];
+//         // }
+//         rna += dna[i] === 'T' ? 'U' : dna[i];
+//     }
+//     return rna;
+    
+// }
+
+// const DNAtoRNA = (dna) => 
+//     dna 
+//         .split('')
+//         .map((el) => (el === 'T' ? 'U' : el))
+//         .join('');
+
+// const DNAtoRNA = (dna) => dna.replace(/T/g, 'U');
+
+const DNAtoRNA = (dna) => dna.replaceAll('T', 'U');
+
+console.log(DNAtoRNA('TTTT'));
