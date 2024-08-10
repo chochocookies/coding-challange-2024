dfb7a59782fcb455c634a7f03780f6579f01eeee
Eko Aryanto <ekoaryanto11@gmail.com>
2024-08-10 23:27:18 +0700

    day 30/365

--- 29-EvenOdd.js
+++ 29-EvenOdd.js
@@ -1,11 +1,20 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-function evenOrOdd(number) {
-    if (number % 2 === 0) {
-        return "Even";
-    } else {
-        return "Odd";
-    }
-}
+// function evenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// }
+
+
+// function evenOrOdd(number) {
+//     return number % 2 === 0 ? 'Even' : 'Odd';
+// }
+
+// arrow func
+const evenOrOdd = (number) => number % 2 === 0 ? 'Even' :evenOrOdd;
 
 console.log(evenOrOdd(7),"Odd");
+
