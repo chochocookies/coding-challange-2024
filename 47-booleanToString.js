759e82519d3848166d0667caeec225b07b35e23f
Eko Aryanto <ekoaryanto11@gmail.com>
2024-08-27 13:57:39 +0700

    day 47/365

---
+++ 47-booleanToString.js
@@ -0,0 +1,11 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// function booleanToString(b){
+//     return b.toString();
+// }
+
+
+
+console.log((false), "false", 'When we pass in false, we want the string "false" as output');
