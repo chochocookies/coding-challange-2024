5ed53f14bc0e0e7d1285653b53ba6400269610ff
Eko Aryanto <ekoaryanto11@gmail.com>
2024-10-04 13:59:08 +0700

    day 86/365

---
+++ Day 51-100/86-howMuchIloveYou.js
@@ -0,0 +1,42 @@
+// following phrases each time a petal was torn:
+
+// "I love you"
+// "a little"
+// "a lot"
+// "passionately"
+// "madly"
+// "not at all"
+// If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+function howMuchILoveYou(nbPetals) {
+    switch ((nbPetals - 1) % 6) {
+        case 1:
+            return "a little";
+        case 2:
+            return "a lot";
+        case 3:
+            return "passionately";
+        case 4:
+            return "madly";
+        case 5:
+            return "not at all";
+        default:
+            return "I love you";
+    }
+}
+function howMuchILoveYou(nbPetals) {
+    const phrases = [
+        "I love you",
+        "a little",
+        "a lot",
+        "passionately",
+        "madly",
+        "not at all"
+    ];
+    
+    return phrases[(nbPetals - 1) % phrases.length];
+}
