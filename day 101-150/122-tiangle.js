69f86f23583c6eb65f554b587f07ce4f9e839b65
Eko Aryanto <ekoaryanto11@gmail.com>
2024-11-12 21:31:18 +0700

    day 123/365

--- day 101-150/122-tiangle.js
+++ day 101-150/122-tiangle.js
@@ -24,4 +24,4 @@ function isTriangle(a,b,c){
     return a + b > c && a + c > b && c + b > a;
 }
 
-// var isTriangle = (a,b,c) => Math.max(a,b,c)<(a+b+c)/2
+// var isTriangle = (   a,b,c) => Math.max(a,b,c)<(a+b+c)/2
